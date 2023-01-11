e29e2ea52c4abc0185c13f204e9239090b8d803e
sguha-work <sguha1988.life@gmail.com>
2023-01-11 23:56:40 +0530

    changes

--- src/components/Parent_component.js
+++ src/components/Parent_component.js
@@ -1,11 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import CounterContext from "../contexts/Counter_context";
 import Child from "./Child_component";
+// The counter context which is created globally will pass the value of 'counter' from parent to every nested children component
 function Parent() {
     const [counter, setCounter] = useState(0);
-    const x = useRef(0);
+    const x = useRef(0); // we have to use useRef here otherwise the value of x will reset to 0 every time the component rerender
     
-    useEffect(() => {
+    useEffect(() => {// this useEffect will be called only once when the component loads for the first time
       window.setInterval(() => {
         x.current = x.current + 1;
         setCounter(x.current);
@@ -13,7 +14,7 @@ function Parent() {
     }, []);
   
     return (
-      <CounterContext.Provider value={counter}>
+      <CounterContext.Provider value={counter}> 
       <div className="App">
         <h1>parent component</h1>
         <label>value--{counter}</label>
